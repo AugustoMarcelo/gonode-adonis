65ce811619dcc8126b927ea37468058051abcd57
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-03 11:22:24 -0300

    Setting relationships in models

--- app/Models/Project.js
+++ app/Models/Project.js
@@ -1,9 +1,21 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Project extends Model {
+  /**
+   * Relationship: a Project belong to a User
+   */
+  user () {
+    return this.belongsTo('App/Models/User')
+  }
+
+  /**
+   * Relationship: a Project has many Tasks
+   */
+  taks () {
+    return this.hasMany('App/Models/Task')
+  }
 }
 
 module.exports = Project
